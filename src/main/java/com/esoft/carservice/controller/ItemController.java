31fb62f255bfdb9fd1a40155c560593e9844cd0d
LakshikaMadhushan <lakshikamadhushan3@gmail.com>
2023-07-12 23:47:51 +0530

    update ItemController

---
+++ src/main/java/com/esoft/carservice/controller/ItemController.java
@@ -0,0 +1,54 @@
+package com.esoft.carservice.controller;
+
+import com.esoft.carservice.dto.common.CommonResponse;
+import com.esoft.carservice.dto.requset.UpdateSaveItemRequestDTO;
+import com.esoft.carservice.service.ItemService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.esoft.carservice.constant.ResponseCodes.OPERATION_SUCCESS;
+import static com.esoft.carservice.constant.ResponseMessages.SUCCESS_RESPONSE;
+
+@Slf4j
+@RestController
+@CrossOrigin
+@RequestMapping("v1/item")
+public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponse> getAllItem() {
+
+        return new ResponseEntity<>(new CommonResponse(OPERATION_SUCCESS, itemService.getAllItem(),
+                SUCCESS_RESPONSE), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{itemId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponse> getItem(@PathVariable long itemId) {
+
+        return new ResponseEntity<>(new CommonResponse(OPERATION_SUCCESS, itemService.getItem(itemId),
+                SUCCESS_RESPONSE), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponse> updateItem(@RequestBody UpdateSaveItemRequestDTO dto) {
+        itemService.updateItem(dto);
+        return new ResponseEntity<>(new CommonResponse(OPERATION_SUCCESS,
+                SUCCESS_RESPONSE), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponse> saveItem(@RequestBody UpdateSaveItemRequestDTO dto) {
+        itemService.saveItem(dto);
+        return new ResponseEntity<>(new CommonResponse(OPERATION_SUCCESS,
+                SUCCESS_RESPONSE), HttpStatus.OK);
+    }
+
+}
