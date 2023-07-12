b4236aa2737ecac90e7c16e80c7a1ff269146107
LakshikaMadhushan <lakshikamadhushan3@gmail.com>
2023-07-13 01:27:14 +0530

    admin and customer crud

---
+++ src/main/java/com/esoft/carservice/controller/AdminController.java
@@ -0,0 +1,54 @@
+package com.esoft.carservice.controller;
+
+import com.esoft.carservice.dto.common.CommonResponse;
+import com.esoft.carservice.dto.requset.UpdateSaveAdminRequestDTO;
+import com.esoft.carservice.service.AdminService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.esoft.carservice.constant.ResponseCodes.OPERATION_SUCCESS;
+import static com.esoft.carservice.constant.ResponseMessages.SUCCESS_RESPONSE;
+
+@Slf4j
+@RestController
+@CrossOrigin
+@RequestMapping("v1/admin")
+public class AdminController {
+    private final AdminService adminService;
+
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponse> getAllAdmin() {
+
+        return new ResponseEntity<>(new CommonResponse(OPERATION_SUCCESS, adminService.getAllAdmin(),
+                SUCCESS_RESPONSE), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{adminId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponse> getAdmin(@PathVariable long adminId) {
+
+        return new ResponseEntity<>(new CommonResponse(OPERATION_SUCCESS, adminService.getAdmin(adminId),
+                SUCCESS_RESPONSE), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponse> updateAdmin(@RequestBody UpdateSaveAdminRequestDTO dto) {
+        adminService.updateAdmin(dto);
+        return new ResponseEntity<>(new CommonResponse(OPERATION_SUCCESS,
+                SUCCESS_RESPONSE), HttpStatus.OK);
+    }
+
+    @PutMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<CommonResponse> saveAdmin(@RequestBody UpdateSaveAdminRequestDTO dto) {
+        adminService.saveAdmin(dto);
+        return new ResponseEntity<>(new CommonResponse(OPERATION_SUCCESS,
+                SUCCESS_RESPONSE), HttpStatus.OK);
+    }
+}
